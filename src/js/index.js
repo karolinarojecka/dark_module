9d5b39f14943501ff2a2b91f14108fd69e19602b
karolinarojecka <k.kasperuk@gmail.com>
2020-06-12 22:13:32 +0200

    update

--- src/js/index.js
+++ src/js/index.js
@@ -3,3 +3,21 @@ import '../scss/main.scss';
 /* place your code below */
 
 console.log('HELLO 🚀')
+
+//change mode
+
+let isDark = true;
+
+const switchMode = document.querySelector('.block__switch');
+
+switchMode.addEventListener('click', () => {
+    if (isDark) {
+        document.documentElement.style.setProperty('--color-background', '#F9F5F1'); 
+        document.documentElement.style.setProperty('--color-text', '#2B1F12'); 
+        isDark = true;
+    } else {
+        document.documentElement.style.setProperty('--color-background', '#2B1F12'); 
+        document.documentElement.style.setProperty('--color-text', '#ffffff'); 
+        isDark = false;
+    }
+})
